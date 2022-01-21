6bec94a5ddb79a6fc173d9a545073f13fa40147c
yordan.todorov@mentormate.com <yordan.todorov@mentormate.com>
2022-01-21 14:43:14 +0200

    ajax

--- myJs.js
+++ myJs.js
@@ -3,4 +3,10 @@ $(function() {
     console.info(document.location.pathname);
 
     $("h1").text("Hello " + document.location.pathname);
+
+    var url = "https://y-pl.azurewebsites.net/Probe?url=" + document.location.pathname.substring(1);
+    $.getJSON( url, function( data ) {
+       console.info(data);
+      });
+
     });
