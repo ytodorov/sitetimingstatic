e5b51c19e5297a5266a86c8c1a94439a8bb955ea
yordan.todorov@mentormate.com <yordan.todorov@mentormate.com>
2022-01-21 19:10:09 +0200

    templates

--- myJs.js
+++ myJs.js
@@ -1,12 +1,27 @@
-$(function() {
+$(function () {
     console.info(document.location);
     console.info(document.location.pathname);
-
-    $("h1").text("Hello " + document.location.pathname);
-
-    var url = "https://y-pl.azurewebsites.net/Probe?url=" + document.location.pathname.substring(1);
-    $.getJSON( url, function( data ) {
-       console.info(data);
-      });
-
-    });
+    if (document.location.pathname != "/") {
+        var url = document.location.pathname.substring(1);
+        var urlToGetData = "https://y-pl.azurewebsites.net/probe?url=" + "google.com";
+        $.getJSON(urlToGetData, function (data) {
+            var htmlToRender = "<table class=\"table\">\n      <thead>\n        <tr>\n          <th scope=\"col\">#</th>\n          <th scope=\"col\">Latency</th>\n          <th scope=\"col\">Dom Load</th>\n          <th scope=\"col\">Load</th>\n        </tr>\n      </thead>\n      <tbody><tr>";
+            debugger;
+            var template = $.templates("#templateProbeRow");
+            var htmlOutput = template.render(data);
+            htmlToRender += htmlOutput;
+            htmlToRender += "</tr></tbody>\n      </table>";
+            $("#singleSiteRow").html(htmlToRender);
+        });
+    }
+    else {
+        $("h1").text("Hello " + document.location.pathname);
+        var url = "https://y-pl.azurewebsites.net/sites?take=19";
+        $.getJSON(url, function (data) {
+            console.info(data);
+            var template = $.templates("#theTmpl");
+            var htmlOutput = template.render(data.$values);
+            $("#sitesRow").html(htmlOutput);
+        });
+    }
+});
---
+++ myJs.ts
@@ -0,0 +1,52 @@
+$(function () {
+  console.info(document.location);
+  console.info(document.location.pathname);
+
+  if (document.location.pathname != "/") {
+
+    var url = document.location.pathname.substring(1);
+    var urlToGetData = "https://y-pl.azurewebsites.net/probe?url=" + "google.com";
+    $.getJSON(urlToGetData, function (data) {
+
+      var htmlToRender = `<table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Latency</th>
+          <th scope="col">Dom Load</th>
+          <th scope="col">Load</th>
+        </tr>
+      </thead>
+      <tbody><tr>`;
+
+      
+      debugger;
+      var template = ($ as any).templates("#templateProbeRow");
+      var htmlOutput = template.render(data);
+
+      htmlToRender += htmlOutput;
+      htmlToRender += `</tr></tbody>
+      </table>`;
+
+      $("#singleSiteRow").html(htmlToRender);
+
+    });
+  }
+  else {
+
+    $("h1").text("Hello " + document.location.pathname);
+
+    var url = "https://y-pl.azurewebsites.net/sites?take=19";
+    $.getJSON(url, function (data) {
+      console.info(data);
+
+      var template = ($ as any).templates("#theTmpl");
+
+      var htmlOutput = template.render(data.$values);
+
+      $("#sitesRow").html(htmlOutput);
+
+    });
+  }
+
+});
