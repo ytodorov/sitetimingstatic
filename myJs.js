c2674d129be946354b074b98504464413ae07b1b
yordan.todorov@mentormate.com <yordan.todorov@mentormate.com>
2022-01-24 13:57:29 +0200

    load fresh data

--- myJs.js
+++ myJs.js
@@ -5,31 +5,17 @@ $(function () {
     if (document.location.pathname != "/") {
         var url = document.location.pathname.substring(1);
         $("h1").text(url);
-        var urlToGetData = "https://y-pl.azurewebsites.net/probes?siteurl=" + url;
-        $.getJSON(urlToGetData, function (data) {
-            var htmlToRender = `<table class="table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Latency</th>
-          <th scope="col">Dom Load</th>
-          <th scope="col">Load</th>
-          <th scope="col">Date Created</th>
-        </tr>
-      </thead>
-      <tbody>`;
-            debugger;
-            var template = $.templates("#templateProbeRow");
-            var htmlOutput = template.render(data);
-            htmlToRender += htmlOutput;
-            htmlToRender += `</tbody>
-      </table>`;
-            $("#singleSiteRow").html(htmlToRender);
+        var urlToGetDataForAllProbes = "https://y-pl.azurewebsites.net/probes?siteurl=" + url;
+        var urlToGetDataForOneProbe = "https://y-pl.azurewebsites.net/probe?url=" + url;
+        $.getJSON(urlToGetDataForOneProbe, function (data) {
+            $.getJSON(urlToGetDataForAllProbes, function (data) {
+                RenderProbesInGrid(data);
+            });
         });
     }
     else {
         $("h1").text("Hello " + document.location.pathname);
-        var url = "https://y-pl.azurewebsites.net/sites?take=19";
+        var url = "https://y-pl.azurewebsites.net/sites?take=27";
         $.getJSON(url, function (data) {
             console.info(data);
             var template = $.templates("#theTmpl");
@@ -38,3 +24,29 @@ $(function () {
         });
     }
 });
+function RenderProbesInGrid(data) {
+    var htmlToRender = `<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Latency</th>
+      <th scope="col">Dom Load</th>
+      <th scope="col">Load</th>
+      <th scope="col">Date Created</th>
+    </tr>
+  </thead>
+  <tbody>`;
+    debugger;
+    var template = $.templates("#templateProbeRow");
+    var htmlOutput = template.render(data);
+    htmlToRender += htmlOutput;
+    htmlToRender += `</tbody>
+  </table>`;
+    $("#singleSiteRow").html(htmlToRender);
+    debugger;
+    if (data[0] && data[0].site && data[0].site.lastProbe) {
+        var template = $.templates("#theTmpl");
+        var htmlOutput = template.render(data.site);
+        $("#sitesRow").html(htmlOutput);
+    }
+}
--- myJs.ts
+++ myJs.ts
@@ -9,41 +9,25 @@ $(function () {
 
     $("h1").text(url);
 
-    var urlToGetData = "https://y-pl.azurewebsites.net/probes?siteurl=" + url;
-    $.getJSON(urlToGetData, function (data) {
+    var urlToGetDataForAllProbes = "https://y-pl.azurewebsites.net/probes?siteurl=" + url;
+    
 
-      var htmlToRender = `<table class="table">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Latency</th>
-          <th scope="col">Dom Load</th>
-          <th scope="col">Load</th>
-          <th scope="col">Date Created</th>
-        </tr>
-      </thead>
-      <tbody>`;
+    var urlToGetDataForOneProbe = "https://y-pl.azurewebsites.net/probe?url=" + url;
+    $.getJSON(urlToGetDataForOneProbe, function (data) {
 
-      
-      debugger;
-      var template = ($ as any).templates("#templateProbeRow");
-      var htmlOutput = template.render(data);
-
-      htmlToRender += htmlOutput;
-      htmlToRender += `</tbody>
-      </table>`;
-
-      $("#singleSiteRow").html(htmlToRender);
-
+      $.getJSON(urlToGetDataForAllProbes, function (data) {
+        RenderProbesInGrid(data);
+      });
     });
+
   }
   else {
 
     $("h1").text("Hello " + document.location.pathname);
 
-    var url = "https://y-pl.azurewebsites.net/sites?take=19";
+    var url = "https://y-pl.azurewebsites.net/sites?take=27";
     $.getJSON(url, function (data) {
-      console.info(data); 
+      console.info(data);
 
       var template = ($ as any).templates("#theTmpl");
 
@@ -55,3 +39,39 @@ $(function () {
   }
 
 });
+
+function RenderProbesInGrid(data: any): void {
+
+  var htmlToRender = `<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Latency</th>
+      <th scope="col">Dom Load</th>
+      <th scope="col">Load</th>
+      <th scope="col">Date Created</th>
+    </tr>
+  </thead>
+  <tbody>`;
+
+
+  debugger;
+  var template = ($ as any).templates("#templateProbeRow");
+  var htmlOutput = template.render(data);
+
+  htmlToRender += htmlOutput;
+  htmlToRender += `</tbody>
+  </table>`;
+
+  $("#singleSiteRow").html(htmlToRender);
+
+  debugger;
+  if (data[0] && data[0].site && data[0].site.lastProbe)
+{
+  var template = ($ as any).templates("#theTmpl");
+
+  var htmlOutput = template.render(data.site);
+
+  $("#sitesRow").html(htmlOutput);
+}
+}
