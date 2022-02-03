7b6beb31012ea0668f38cab5d0e044baa24303ca
yordan.todorov@mentormate.com <yordan.todorov@mentormate.com>
2022-02-03 14:49:16 +0200

    cards

--- singleSite.ts
+++ singleSite.ts
@@ -114,25 +114,30 @@ $(window).resize(function(){
     }
 });
 
-
-$("#skeletonwestus3").kendoSkeletonContainer({
-    animation: "pulse",
-    template: `<div class="k-card">
-                        <div class="k-card-header">
-                            <div>
-                                <span data-shape-circle class="k-card-image avatar"></span>
+$(".skeletonMarker").each(function( index )
+{
+    $(this).kendoSkeletonContainer({
+        animation: "pulse",
+        template: `<div class="k-card">
+                            <div class="k-card-header">
+                                <div>
+                                    <span data-shape-circle class="k-card-image avatar"></span>
+                                </div>
+                                <div class="user-info" style="width: 100%;">
+                                    <span data-shape-text class='k-card-title'></span>
+                                    <span data-shape-text class='k-card-subtitle' style="width: 35%;"></span>
+                                </div>
                             </div>
-                            <div class="user-info" style="width: 100%;">
-                                <span data-shape-text class='k-card-title'></span>
-                                <span data-shape-text class='k-card-subtitle' style="width: 35%;"></span>
+                            <span data-shape-rectangle style="width: 340px; height: 100%;"></span>
+                            <div class="k-card-body">
+                                <span data-shape-text></span>
                             </div>
-                        </div>
-                        <span data-shape-rectangle style="width: 340px; height: 100%;"></span>
-                        <div class="k-card-body">
-                            <span data-shape-text></span>
-                        </div>
-                    </div>`
+                        </div>`
+    });
 });
+
+
+
 
 var urlwestus3 = `https://st-westus3.azurewebsites.net/probe?url=${url}`;
 var urleastus2 = `https://st-eastus2.azurewebsites.net/probe?url=${url}`;
@@ -140,24 +145,59 @@ var urlsoutheastasia = `https://st-southeastasia.azurewebsites.net/probe?url=${u
 
 $.get(urlwestus3, function (data) {
     $("#skeletonwestus3").remove();
-    $("#cards").html(
+    $("#cards").append(
         ` <div class="k-card">
-        <div class="k-card-header">
-            <div>
-                <img class="k-card-image avatar" src="https://demos.telerik.com/kendo-ui/content/web/skeleton/avatar.jpg" />
-            </div>
-            <div class="user-info">
-                <h5 class="k-card-title">latencyInChrome: ${data.latencyInChrome}</h5>
-                <h6 class="k-card-subtitle">${data.dateCreatedAgo}</h6>
-            </div>
-        </div>
-        <img class="k-card-image" src="https://sitetiming.blob.core.windows.net/images/short5_${data.uniqueGuid}.jpeg?sv=2020-08-04&st=2012-01-27T12%3A30%3A00Z&se=2032-01-28T12%3A30%3A00Z&sr=c&sp=rl&sig=jvKd8yqdiz42u28l4oPYHVFWUSCaeLYmeKMMCgwtn1Y%3D" />
+        <img class="k-card-image" onerror="if (this.src != 'https://static8.depositphotos.com/1010782/858/v/600/depositphotos_8584590-stock-illustration-website-maintenance-message.jpg') this.src = 'https://static8.depositphotos.com/1010782/858/v/600/depositphotos_8584590-stock-illustration-website-maintenance-message.jpg';" src="https://sitetiming.blob.core.windows.net/images/short5_${data.uniqueGuid}.jpeg?sv=2020-08-04&st=2012-01-27T12%3A30%3A00Z&se=2032-01-28T12%3A30%3A00Z&sr=c&sp=rl&sig=jvKd8yqdiz42u28l4oPYHVFWUSCaeLYmeKMMCgwtn1Y%3D" />
         <div class="k-card-body">
-            <p>dOMContentLoadedEventInChrome: ${data.dOMContentLoadedEventInChrome}</p>
+            <h6 class="k-card-subtitle">Latency: ${data.latencyInChrome}</h6>
+            <h6 class="k-card-subtitle">DOM Loaded: ${data.domContentLoadedEventInChrome}</h6>
+            <h6 class="k-card-subtitle">Latency: ${data.sourceIpAddress}</h6>
+            <h6 class="k-card-subtitle">Error: ${data.exceptionMessage}</h6>
         </div>
     </div>`
     );
-    console.log(data)
+
+    var kendoChart = $("#chart").data("kendoChart");
+    kendoChart?.dataSource.fetch();
+    console.log("urlwestus3");
+    console.log(data);
 });
-$.get(urleastus2, function (data) {console.log(data)});
-$.get(urlsoutheastasia, function (data) {console.log(data)});
+$.get(urleastus2, function (data) {
+    $("#skeletoneastus2").remove();
+    $("#cards").append(
+        ` <div class="k-card">
+        <img class="k-card-image" onerror="if (this.src != 'https://static8.depositphotos.com/1010782/858/v/600/depositphotos_8584590-stock-illustration-website-maintenance-message.jpg') this.src = 'https://static8.depositphotos.com/1010782/858/v/600/depositphotos_8584590-stock-illustration-website-maintenance-message.jpg';" src="https://sitetiming.blob.core.windows.net/images/short5_${data.uniqueGuid}.jpeg?sv=2020-08-04&st=2012-01-27T12%3A30%3A00Z&se=2032-01-28T12%3A30%3A00Z&sr=c&sp=rl&sig=jvKd8yqdiz42u28l4oPYHVFWUSCaeLYmeKMMCgwtn1Y%3D" />
+        <div class="k-card-body">
+            <h6 class="k-card-subtitle">Latency: ${data.latencyInChrome}</h6>
+            <h6 class="k-card-subtitle">DOM Loaded: ${data.domContentLoadedEventInChrome}</h6>
+            <h6 class="k-card-subtitle">Latency: ${data.sourceIpAddress}</h6>
+            <h6 class="k-card-subtitle">Error: ${data.exceptionMessage}</h6>
+        </div>
+    </div>`
+    );
+
+    var kendoChart = $("#chart").data("kendoChart");
+    kendoChart?.dataSource.fetch();
+    console.log("urleastus2");
+    console.log(data);
+    
+});
+$.get(urlsoutheastasia, function (data) {
+    $("#skeletonsoutheastasia").remove();
+    $("#cards").append(
+        ` <div class="k-card">
+        <img class="k-card-image" onerror="if (this.src != 'https://static8.depositphotos.com/1010782/858/v/600/depositphotos_8584590-stock-illustration-website-maintenance-message.jpg') this.src = 'https://static8.depositphotos.com/1010782/858/v/600/depositphotos_8584590-stock-illustration-website-maintenance-message.jpg';" src="https://sitetiming.blob.core.windows.net/images/short5_${data.uniqueGuid}.jpeg?sv=2020-08-04&st=2012-01-27T12%3A30%3A00Z&se=2032-01-28T12%3A30%3A00Z&sr=c&sp=rl&sig=jvKd8yqdiz42u28l4oPYHVFWUSCaeLYmeKMMCgwtn1Y%3D" />
+        <div class="k-card-body">
+            <h6 class="k-card-subtitle">Latency: ${data.latencyInChrome}</h6>
+            <h6 class="k-card-subtitle">DOM Loaded: ${data.domContentLoadedEventInChrome}</h6>
+            <h6 class="k-card-subtitle">Latency: ${data.sourceIpAddress}</h6>
+            <h6 class="k-card-subtitle">Error: ${data.exceptionMessage}</h6>
+        </div>
+    </div>`
+    );
+
+    var kendoChart = $("#chart").data("kendoChart");
+    kendoChart?.dataSource.fetch();
+    console.log("urlsoutheastasia");
+    console.log(data);
+});
